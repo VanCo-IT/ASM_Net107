5f03974bdadb9f55c41253634c46ff3041648946
vanCo-IT <colv51614@gmail.com>
2026-07-11 10:50:09 +0700

    done validation Models-PhieuBanHang

--- WebCafePoly/Models/PhieuBanHang.cs
+++ WebCafePoly/Models/PhieuBanHang.cs
@@ -12,20 +12,30 @@ public partial class PhieuBanHang
     [Key]
     [StringLength(6)]
     [Unicode(false)]
+    [Display(Name = "Mã phiếu")]
+    [Required(ErrorMessage = "Không được để trống mã phiếu")]
     public string MaPhieu { get; set; } = null!;
 
     [StringLength(50)]
+    [Display(Name = "Mã khách hàng")]
+    [Required(ErrorMessage = "Không được để trống mã khách hàng")]
     public string? MaKhachHang { get; set; }
 
     [StringLength(6)]
     [Unicode(false)]
+    [Display(Name = "Mã thẻ")]
+    [Required(ErrorMessage = "Không được để trống mã thẻ")]
     public string? MaThe { get; set; }
 
     [StringLength(6)]
     [Unicode(false)]
+    [Display(Name = "Mã nhân viên")]
+    [Required(ErrorMessage = "Không được để trống mã nhân viên")]
     public string? MaNhanVien { get; set; }
 
     [Column(TypeName = "datetime")]
+    [Display(Name = "Thời gian")]
+    [Required(ErrorMessage = "Không được để trống thời gian")]
     public DateTime NgayTao { get; set; }
 
     public bool TrangThai { get; set; }
